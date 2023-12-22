65113c4b4221cfbf196cf1e95b3c838592dc4eaf
kirtimahadane <kirti.mahadane@gmail.com>
2023-12-22 14:01:45 +0530

    completed mini project

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!firstName || firstName === "") {
+      return;
+    }
+    if (!lastName || lastName === "") {
+      return;
+    }
+    return setFullName(`Full Name: ${firstName} ${lastName}`);
+  };
+  return (
+    <div className="App">
+      <h1>Full Name Display</h1>
+      <form>
+        <input
+          type="text"
+          required
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <br />
+        <input
+          type="text"
+          required
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <br />
+        <button type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+      <p>{fullName}</p>
+    </div>
+  );
+}
