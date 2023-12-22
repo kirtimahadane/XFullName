577b6825d39cdf3f673d2e487676c2da835a3046
kirtimahadane <kirti.mahadane@gmail.com>
2023-12-22 20:08:26 +0530

    label is added

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ export default function App() {
   const [fullName, setFullName] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
-    // if (!firstName || firstName === "") {
-    //   return;
-    // }
-    // if (!lastName || lastName === "") {
-    //   return;
-    // }
+    if (!firstName ) {
+      setFirstName("Please fill out  this field")
+      return;
+    }
+    if (!lastName) {
+      setLastName("Please fill  out this field")
+      return;
+    }
     return setFullName(`Full Name: ${firstName} ${lastName}`);
     
   };
@@ -20,18 +22,22 @@ export default function App() {
     <div className="App">
       <h1>Full Name Display</h1>
       <form>
+      <label htmlFor="firstName">First Name:</label>
+
         <input
           type="text"          
           value={firstName}
-          required
           onChange={(e) => setFirstName(e.target.value)}
+          required
         />
         <br />
+        <label htmlFor="lastName">First Name:</label>
+
         <input
           type="text"          
           value={lastName}
-          required
           onChange={(e) => setLastName(e.target.value)}
+          required
         />
         <br />
         <button type="submit" onClick={handleSubmit}>
